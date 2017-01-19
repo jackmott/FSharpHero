d33bae4bca8ef5dc88d195ea594ebb503b438bb9
jack mott <jack.mott@gmail.com>
2017-01-19 09:46:19 -0600

    day 5

--- fsh/sdl_fsh.fs
+++ fsh/sdl_fsh.fs
@@ -7,45 +7,82 @@ module SDL_FSH =
     open Microsoft.FSharp.NativeInterop
     open System
 
-    let mutable pixels = Array.zeroCreate<int32> 0
-    let mutable texture = Unchecked.defaultof<nativeint>
-    let mutable textureWidth = Unchecked.defaultof<int>
-    let mutable textureHeight = Unchecked.defaultof<int>
+    type SDL_Texture = nativeint
+    type SDL_Window = nativeint
+    type SDL_Renderer = nativeint
 
-    let RenderWeirdGradient (boffset:int32) (goffset:int32) =        
+    type SDL_OffscreenBuffer = 
+        {
+            mutable Memory : int32[]
+            mutable Texture : SDL_Texture
+            mutable Width : int32
+            mutable Height : int32
+            mutable Pitch : int
+        }
+
+    type SDL_WindowSize =
+        {
+            Width : int
+            Height : int
+        }
+    
+    let GlobalBackBuffer =
+        {
+            Memory = Array.zeroCreate<int32> 0
+            Texture = IntPtr.Zero
+            Width = 0
+            Height = 0
+            Pitch = 0
+        }
+
+    let GetWindowSize (window : SDL_Window) = 
+        let mutable w = 0
+        let mutable h = 0
+        SDL.SDL_GetWindowSize(window,&w,&h)
+        {
+            Width = w
+            Height = h
+        }
+
+    
+    let RenderWeirdGradient (buffer:SDL_OffscreenBuffer) (bOffset:int32) (gOffset:int32) =        
         let mutable row = 0
-        for y in 0 .. textureHeight-1 do
-            for x in 0 .. textureWidth-1 do
-                let b = (x + boffset) % 256
-                let g = (y + goffset) % 256
-                pixels.[row+x] <- int32(g <<< 8 ||| b)
-            row <- row+textureWidth
+        let w = buffer.Width
+        let h = buffer.Height
+        for y in 0 .. h-1 do
+            for x in 0 .. w-1 do
+                let b = (x + bOffset) % 256
+                let g = (y + gOffset) % 256
+                buffer.Memory.[row+x] <- int32(g <<< 8 ||| b)
+            row <- row+w
 
 
-    let CreateTexture (window:nativeint) =        
-        let mutable Width = Unchecked.defaultof<int>
-        let mutable Height = Unchecked.defaultof<int>
-        SDL.SDL_GetWindowSize(window,&Width,&Height)
-        let renderer = SDL.SDL_GetRenderer(window)
-        if texture <> Unchecked.defaultof<nativeint> then
-            SDL.SDL_DestroyTexture(texture) |> ignore
-        texture <- SDL.SDL_CreateTexture(renderer,
-                                         SDL.SDL_PIXELFORMAT_ARGB8888,
-                                         (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
-                                         Width,
-                                         Height)
-        pixels <- Array.zeroCreate (Width*Height)
-        textureWidth <- Width
-        textureHeight <- Height
+    let ResizeTexture (buffer : SDL_OffscreenBuffer) (renderer : SDL_Renderer) (w:int) (h:int) =        
+                        
+        if buffer.Texture <> IntPtr.Zero then
+            SDL.SDL_DestroyTexture(buffer.Texture) |> ignore        
+        buffer.Memory <- Array.zeroCreate (w*h)
+        buffer.Texture <- SDL.SDL_CreateTexture(renderer,
+                                                SDL.SDL_PIXELFORMAT_ARGB8888,
+                                                (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
+                                                w,h)
+        printfn "%A" (SDL.SDL_GetError())
+        buffer.Width <- w
+        buffer.Height <- h
+        buffer.Pitch <- 4*w
+        
+        
 
-    let UpdateWindow (window:nativeint) (renderer:nativeint) =
-        let pixelHandle = GCHandle.Alloc(pixels,GCHandleType.Pinned)
+    let UpdateWindow (window:SDL_Window) (renderer:SDL_Renderer) (buffer:SDL_OffscreenBuffer) =
+        let pixelHandle = GCHandle.Alloc(buffer.Memory,GCHandleType.Pinned)
         let pixelPtr = pixelHandle.AddrOfPinnedObject()                
         
-        if SDL.SDL_UpdateTexture(texture,IntPtr.Zero,pixelPtr,textureWidth*4) <> 0 then
+        if SDL.SDL_UpdateTexture(buffer.Texture,
+                                 IntPtr.Zero,pixelPtr,
+                                 buffer.Pitch) <> 0 then
             printfn "UpdateTexture Error"
                 
-        if SDL.SDL_RenderCopy(renderer,texture,IntPtr.Zero,IntPtr.Zero) <> 0 then
+        if SDL.SDL_RenderCopy(renderer,buffer.Texture,IntPtr.Zero,IntPtr.Zero) <> 0 then
             printfn "RenderCopy Error"
 
         SDL.SDL_RenderPresent(renderer)
@@ -61,16 +98,13 @@ module SDL_FSH =
             -> printfn "Window Event"
                match event.window.windowEvent with
                | SDL.SDL_WindowEventID.SDL_WINDOWEVENT_SIZE_CHANGED               
-                    -> let window = SDL.SDL_GetWindowFromID(event.window.windowID)
-                       let renderer = SDL.SDL_GetRenderer(window)
-                       CreateTexture window 
-                       false
+                    -> false
                | SDL.SDL_WindowEventID.SDL_WINDOWEVENT_FOCUS_GAINED
                     -> false
                | SDL.SDL_WindowEventID.SDL_WINDOWEVENT_EXPOSED            
                     -> let window = SDL.SDL_GetWindowFromID(event.window.windowID)
                        let renderer = SDL.SDL_GetRenderer(window)
-                       UpdateWindow window renderer
+                       UpdateWindow window renderer GlobalBackBuffer
                        false
                | _  -> false
         | _ -> false
@@ -83,13 +117,14 @@ module SDL_FSH =
             printfn "error on init"
         else
             printfn "success"
-            let windowHandle =
+            let window =
                 SDL.SDL_CreateWindow("FSharp Hero",
                                         100,100,1920,1200,
                                         SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE)
         
-            let renderer = SDL.SDL_CreateRenderer(windowHandle,-1,SDL.SDL_RendererFlags.SDL_RENDERER_SOFTWARE) 
-            CreateTexture windowHandle
+            let renderer = SDL.SDL_CreateRenderer(window,-1,SDL.SDL_RendererFlags.SDL_RENDERER_SOFTWARE) 
+            let windowSize = GetWindowSize window           
+            ResizeTexture GlobalBackBuffer renderer windowSize.Width windowSize.Height
             let mutable quit = false;
             let mutable xoff = 0;
             let mutable yoff = 0;
@@ -100,8 +135,8 @@ module SDL_FSH =
                         quit <- true
                     else         
                         quit <- HandleEvent(event)
-                RenderWeirdGradient xoff yoff
-                UpdateWindow windowHandle renderer
+                RenderWeirdGradient GlobalBackBuffer xoff yoff
+                UpdateWindow window renderer GlobalBackBuffer
                 yoff <- yoff + 2
                 xoff <- xoff + 1
                 
